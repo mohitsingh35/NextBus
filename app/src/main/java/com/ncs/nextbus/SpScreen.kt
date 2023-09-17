bfbff516e75e04e26c7f10c8524f151b86242d5e
Ashita <ashitam2407@gmail.com>
2023-09-17 20:44:41 +0530

    from ashita

--- app/src/main/java/com/ncs/nextbus/SpScreen.kt
+++ app/src/main/java/com/ncs/nextbus/SpScreen.kt
@@ -32,13 +32,13 @@ class SpScreen : AppCompatActivity(){
 //            finish()
 //        }
         logo.animate().setDuration(1500).alpha(1f).withEndAction{
-            val i= Intent(this,MainActivity::class.java)
+            val i= Intent(this,FrontScreen::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }
         tagline.animate().setDuration(1500).alpha(1f).withEndAction{
-            val i= Intent(this,MainActivity::class.java)
+            val i= Intent(this,FrontScreen::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
