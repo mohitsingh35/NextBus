c06fada1362d74b452e74a01f00369dd0247bf92
mohit <mohitsingh732@yahoo.com>
2023-09-17 23:41:12 +0530

    Location Tracking enabled

--- app/src/main/java/com/ncs/nextbus/RealtimeDB.kt
+++ app/src/main/java/com/ncs/nextbus/RealtimeDB.kt
@@ -11,12 +11,22 @@ data class RealtimeDB(
     data class locationData(
         val startinglat:Double?=null,
         val startinglong:Double?=null,
-        val endinglat:Double?=null,
+        val endinglat:Double?= null,
         val endinglong:Double?=null,
         val accuracy: Double?=null,
         val latitude: Double?=null,
         val longitude: Double?=null,
         val speed: Double?=null,
         val time: Long?=null,
+        val busNum:String?="",
+        val busID:String?="",
+        val fueltype:String?="",
+        val start:String?="",
+        val destination:String?="",
+        val driverName:String?="",
+        val driverphNum:String?="",
+        val driverprofilepic:String?="",
+        val driverId:String?="",
+        val busName:String?="",
     )
 }
